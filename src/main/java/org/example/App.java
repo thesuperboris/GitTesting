4a53af3918d3876986fd5260d8d9237ca4c38681
unknown <michaelberlin96@gmail.com>
2023-04-06 10:32:47 -0400

    Added changes to App in main/java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        System.out.println("New changes for Git");
+        //We added this file already, but now we modified it.
+        //This shows when we do git status again
     }
+
 }
