e259a7ae475ae57029c8b1ce0549cae0351b8b74
Michael Berlin <michaelberlin96@gmail.com>
2023-04-10 11:30:24 -0400

    Added changes to App2

--- src/main/java/org/example/App2.java
+++ src/main/java/org/example/App2.java
@@ -3,6 +3,8 @@ package org.example;
 public class App2 {
 
     public static void main (String[] args){
-        System.out.println("New File");
+        System.out.println("New Filelll");
+
+        //Changes again
     }
 }
